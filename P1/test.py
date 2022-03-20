90e98fc4c22291649e1d8993533cbdeba6d639dd
tjwjdgus12 <01048733438a@gmail.com>
2022-03-20 23:06:50 +0900

    Update UI, Add patch_size variable

--- P1/test.py
+++ P1/test.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 
-title = 'Project 1' 
+PATCH_SIZE = 30
+
+title = 'Project 1'
 
 img1 = cv2.imread('P1/1st.jpg')
 img2 = cv2.imread('P1/2nd.jpg')
@@ -9,10 +11,11 @@ img2 = cv2.imread('P1/2nd.jpg')
 img1 = cv2.resize(img1, (600, 800))
 img2 = cv2.resize(img2, (600, 800))
 
+img1_rois = []
+
 def onMouse(event, x, y, flags, param):
-    print(event, x, y, )
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.rectangle(img1, (x-10, y-10), (x+10, y+10), (255, 255, 255), -1)
+        cv2.rectangle(img1, (x-PATCH_SIZE//2, y-PATCH_SIZE//2), (x+PATCH_SIZE//2, y+PATCH_SIZE//2), (0, 0, 255), 2)
         cv2.imshow(title, img1)
 
 cv2.imshow(title, img1)
