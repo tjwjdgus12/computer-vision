78389e97de7f435f90efd43072b22c0311b58b07
joon <sh05158@flysher.net>
2022-03-21 00:02:56 +0900

    히스토그램 보여주는 함수 작업

--- P1/test.py
+++ P1/test.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 
 PATCH_SIZE = 30
-
+COLOR = ('b','g','r')
 title = 'Project 1'
 
 img1 = cv2.imread(os.path.join(os.getcwd(),'P1/1st.jpg'))
@@ -12,7 +13,12 @@ img2 = cv2.imread(os.path.join(os.getcwd(),'P1/2nd.jpg'))
 img1 = cv2.resize(img1, (600, 800))
 img2 = cv2.resize(img2, (600, 800))
 
-
+def showHistogram(arr):
+    for i,col in enumerate(COLOR):
+        histr = cv2.calcHist([arr],[i],None,[256],[0,256])
+        plt.plot(histr,color = col)
+        plt.xlim([0,256])
+    plt.show()
 
 def onMouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
