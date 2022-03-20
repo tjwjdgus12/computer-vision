f4640462a1fcc99f27e9a198137b74f55b8cc116
joon <sh05158@flysher.net>
2022-03-21 02:33:17 +0900

    히스토그램 patch 별 bar 출력 구현

--- P1/test.py
+++ P1/test.py
@@ -17,26 +17,42 @@ img2 = cv2.resize(img2, (300, 400))
 roi1, roi2 = [], []
 roi_pos1, roi_pos2 = [], []
 
-def showHistogram(arr):
-    # for i,col in enumerate(COLOR):
+patchHistogram = []
+
+def displayPatchHistogram(rowcount=2, colcount=4):
+    fig, axes = plt.subplots(nrows=rowcount, ncols=colcount)
     
-    histr = cv2.calcHist([arr],[0],None,[32],[0,256])
-    # histogramBin = np.bincount(histr.ravel(),minlength=256)
-    # print(histogramBin,len(histogramBin))
-    plt.bar(np.arange(32),histr.ravel())
-    print(histr)
-    # plt.hist(histr)
-    # plt.xlim([0,256])
+    flattenArr = axes.flatten()
     
+    print(patchHistogram)
+    for i in range(0,rowcount):
+        for j in range(0,colcount):
+            
+            axes[i][j].bar(np.arange(32), patchHistogram[i*colcount+j].ravel())
+            axes[i][j].set_title('Picture '+str(i)+ '/ Patch '+str(j))
+            
+    fig.tight_layout()
     plt.show()
+    
+# displayPatchHistogram()
+    
+def getHistogram(arr):
+    histr = cv2.calcHist([arr],[0],None,[32],[0,256])
+    
+    return histr
+    
+    # plt.bar(np.arange(32),histr.ravel())
+    # plt.show()
     
 def diff2PatchArray(patchArr1, patchArr2):
     hists1 = []
     hists2 = []
+    
+    
     for patch in patchArr1:
         # hsv = cv2.cvtColor(patch, cv2.COLOR_BGR2HSV)
         # hist = cv2.calcHist([hsv], [0,1], None, [180,256], [0,180,0, 256])
-        showHistogram(patch)
+        patchHistogram.append( getHistogram(patch) )
         hist = cv2.calcHist([patch],[0],None,[256],[0,256])
         
         hists1.append(hist)
@@ -44,6 +60,8 @@ def diff2PatchArray(patchArr1, patchArr2):
     for patch in patchArr2:
         # hsv = cv2.cvtColor(patch, cv2.COLOR_BGR2HSV)
         # hist = cv2.calcHist([hsv], [0,1], None, [180,256], [0,180,0, 256])
+        patchHistogram.append( getHistogram(patch) )
+        
         hist = cv2.calcHist([patch],[0],None,[256],[0,256])
         
         hists2.append(hist)
@@ -65,9 +83,11 @@ def onMouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         x1, x2 = x - PATCH_SIZE // 2, x + PATCH_SIZE // 2
         y1, y2 = y - PATCH_SIZE // 2, y + PATCH_SIZE // 2
-        cv2.rectangle(param[0], (x1, y1), (x2, y2), (0, 0, 255), 1)
-        param[1].append(param[0][y1:y2+1, x1:x2+1])
+        param[1].append(param[0][y1:y2+1, x1:x2+1].copy())
         param[2].append((x, y))
+        
+        
+        cv2.rectangle(param[0], (x1, y1), (x2, y2), (0, 0, 255), 1)
         cv2.putText(param[0], str(len(param[1])), (x-5, y-10), \
                             cv2.FONT_HERSHEY_DUPLEX, 0.5, (0,0,255))
         cv2.imshow(title, param[0])
@@ -85,6 +105,7 @@ cv2.waitKey()
 cv2.destroyAllWindows()
 
 result = diff2PatchArray(roi1, roi2)
+displayPatchHistogram(2, len(roi1))
 
 img = np.hstack((img1, img2))
 pair = np.argmax(result, axis=1)
