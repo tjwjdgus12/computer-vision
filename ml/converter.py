823d2d8c9e6e40e81eb8197d7d35d047aa6c51d5
joon <sh05158@flysher.net>
2022-06-08 02:22:42 +0900

    dd

---
+++ ml/converter.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+WIDTH = 272
+HEIGHT = 136
+BALL_RADIUS = 3
+
+def str2img(string):
+    string = string.replace(',', ' ')
+    ball_pos = list(map(int, string.split()))
+
+    white_ball_pos = (ball_pos[0]*4, ball_pos[1]*4)
+    red_ball_pos = (ball_pos[2]*4, ball_pos[3]*4)
+    yellow_ball_pos = (ball_pos[4]*4, ball_pos[5]*4)
+
+    table = np.zeros((HEIGHT*4, WIDTH*4, 3), dtype=np.float32)
+
+    cv2.circle(table, white_ball_pos, BALL_RADIUS*4, (255,255,255), -1)
+    cv2.circle(table, red_ball_pos, BALL_RADIUS*4, (0,0,255), -1)
+    cv2.circle(table, yellow_ball_pos, BALL_RADIUS*4, (0,255,255), -1)
+
+    return table
+
+
+if __name__ == "__main__":
+
+    while True:
+        ball_pos_str = input()
+        if ball_pos_str == "": continue
+
+        table_img = str2img(ball_pos_str)
+        
+        cv2.imshow("result", table_img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
--- ml/learner.py
+++ ml/learner.py
@@ -1,13 +1,11 @@
 import numpy as np
-
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 from tensorflow.keras.utils import to_categorical
-
-import itertools as it
-
 from sklearn.preprocessing import LabelEncoder
+import itertools as it
 
 WIDTH = 272
 HEIGHT = 136
@@ -17,26 +15,22 @@ BALL_RADIUS = 3
 np.random.seed(5)
 tf.random.set_seed(5)
 
+
 # 1. 데이터 준비하기
 import pandas as pd
 dataset = pd.read_csv("data.csv", delimiter=",")
 dataset = dataset.values
 
 # 2. 데이터셋 생성하기 : 700
-x_train = dataset[:100,0:6]
-y_train = dataset[:100,6]
-x_test = dataset[100:,0:6]
-y_test = dataset[100:,6]
+x_train = dataset[:200,0:6]
+y_train = dataset[:200,6]
+x_test = dataset[200:,0:6]
+y_test = dataset[200:,6]
 
 def getFlipNumber(n):
     d = {0:1,1:0,2:3,3:2,4:4}
     return d[n]
 
-# print(x_train)
-# print(y_train)
-
-# print(x_train.shape)
-# print(y_train.shape)
 
 x_train_copy = x_train.copy()
 y_train_copy = y_train.copy()
@@ -44,18 +38,22 @@ y_train_copy = y_train.copy()
 x_train_iter = x_train_copy.copy()
 y_train_iter = y_train_copy.copy()
 
+# x_temp = []
+# y_temp = []
 
+# from tqdm import tqdm
+# for k, x in enumerate(x_train_copy):
+#     iteration = it.product([-1,0,1],repeat=6)
+#     for iters in tqdm(iteration):
+#         x_temp.append([x[i]])
 
-for k, x in enumerate(x_train_copy):
-    iteration = it.product([-1,0,1],repeat=6)
-    for iters in iteration:
-        new_x = x.copy()
+#         new_x = x.copy()
 
-        for i, iter in enumerate(iters):
-            new_x[i] = new_x[i] + iter
+#         for i, iter in enumerate(iters):
+#             new_x[i] = new_x[i] + iter
 
-        x_train_iter= np.vstack([x_train_iter,new_x])
-        y_train_iter= np.append(y_train_iter,y_train_iter[k])
+#         x_train_iter= np.vstack([x_train_iter,new_x])
+#         y_train_iter= np.append(y_train_iter,y_train_iter[k])
 
 
 x_train = x_train_iter.copy()
@@ -63,9 +61,6 @@ y_train = y_train_iter.copy()
 
 x_train_iter_copy = x_train_iter.copy()
 y_train_iter_copy = y_train_iter.copy()
-
-# x_train_xflip = x_train_copy.copy()
-# y_train_xflip = y_train_copy.copy()
 
 x_train_xflip = x_train_iter_copy.copy()
 y_train_xflip = y_train_iter_copy.copy()
@@ -87,7 +82,6 @@ for i in range(y_train_yflip.shape[0]):
     y_train_yflip[i] = getFlipNumber(y_train_yflip[i])
 
 
-
 x_train_flip = x_train_iter_copy.copy()
 y_train_flip = y_train_iter_copy.copy()
 
@@ -105,16 +99,19 @@ y_train = np.concatenate([y_train, y_train_xflip, y_train_yflip, y_train_flip])
 
 #######################
 
+
+
 encoder = LabelEncoder()
 encoder.fit(y_train)
 y_encoded = to_categorical(encoder.transform(y_train))
 
 # 3. 모델 구성하기
 model = Sequential()
-model.add(Dense(12, input_dim=6, activation='relu'))
-model.add(Dense(6, activation='relu'))
-model.add(Dense(6, activation='relu'))
-model.add(Dense(6, activation='relu'))
+model.add(Dense(24, input_dim=6, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(24, activation='relu'))
 
 model.add(Dense(5, activation='softmax'))
 
@@ -123,7 +120,8 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 
 
 # 5. 모델 학습시키기
-model.fit(x_train, y_encoded, epochs=50, batch_size=64)
+model.fit(x_train, y_encoded, epochs=1500, batch_size=32)
+model.summary()
 
 # 6. 모델 평가하기
 # scores = model.evaluate(x_test, y_test)
@@ -143,3 +141,7 @@ for pre in pred:
     t = [(i, p) for i, p in enumerate(pre)]
     t.sort(key=lambda x: x[1], reverse=True)
     print(t)
+
+model_file_name = input("[save] Input model name: ")
+if model_file_name != "":
+    model.save(model_file_name)
